ea492073891a40fba8ea48a6aeb67a3ee3b16223
[aviv polak] <[avivp0437@gmail.com]>
2021-12-18 16:49:42 +0200

    register and log in succsses

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
@@ -9,13 +10,33 @@ const App = () => {
     const [errorMessage, setErrorMessage] = useState(null);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
 
     useEffect(() => {
         blogService.getAll().then((blogs) => setBlogs(blogs));
     }, []);
-    const handleLogin = () => {
-        console.log("handle login");
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const { body: user } = await axios.post("api/users/login", {
+            username,
+            password,
+        });
+        console.log(user);
     };
+    const handleSingup = async (e) => {
+        e.preventDefault();
+        console.log("sing");
+        const { data: user } = await axios.post("/api/users/register", {
+            username,
+            password,
+            name,
+        });
+        console.log("up");
+
+        console.log(user);
+    };
+
     const handleBlogChange = () => {
         console.log("handleBlogChange");
     };
@@ -25,6 +46,7 @@ const App = () => {
 
     const loginForm = () => (
         <form onSubmit={handleLogin}>
+            <h2>login</h2>
             <div>
                 username
                 <input
@@ -46,6 +68,39 @@ const App = () => {
             <button type="submit">login</button>
         </form>
     );
+    const singupForm = () => (
+        <form onSubmit={handleSingup}>
+            <h2>sing-up</h2>
+            <div>
+                username
+                <input
+                    type="text"
+                    value={username}
+                    name="Username"
+                    onChange={({ target }) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+                name
+                <input
+                    type="text"
+                    value={name}
+                    name="name"
+                    onChange={({ target }) => setName(target.value)}
+                />
+            </div>
+            <div>
+                password
+                <input
+                    type="password"
+                    value={password}
+                    name="Password"
+                    onChange={({ target }) => setPassword(target.value)}
+                />
+            </div>
+            <button type="submit">singup</button>
+        </form>
+    );
 
     const blogForm = () => (
         <form onSubmit={addBlog}>
@@ -57,6 +112,7 @@ const App = () => {
     return (
         <div>
             {username !== null ? loginForm() : blogForm()}
+            {singupForm()}
             <h2>blogs</h2>
             {blogs.map((blog) => (
                 <Blog key={blog.id} blog={blog} />
